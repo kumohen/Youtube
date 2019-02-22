e24efd497b2a6d6d5c5a398a2163073f9fba571b
mahenmondal <mahenmondal111@gmail.com>
2019-02-22 21:22:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from './Components/search_bar';
+import VideoList from './Components/video_list';
+import VideoDetail from './Components/video_details';
+import Youtube from './Components/header';
+import './style/style.css';
+import YTSearch from 'youtube-api-search';
 
-class App extends Component {
-  render() {
+const API_KEY = 'YOUR_API_KEY';
+
+
+
+
+
+class App extends Component{
+   constructor(props){
+        super(props);
+        this.state = {videos : [],
+          selectedVideo:null
+        }
+
+        this.videoSearch('surfboards');
+
+    }
+
+
+    videoSearch(term){
+      YTSearch({key:API_KEY ,term:term},(videos)=>{
+                this.setState({videos:videos,
+                  selectedVideo:videos[0]
+                })
+        });
+    }
+
+  render(){
+    const videoSearch = _.debounce((term)=> {this.videoSearch(term)},300);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Youtube />
+        <SearchBar
+        onSearchTermChange = {videoSearch}
+        />
+        <VideoDetail video={this.state.selectedVideo}  />
+        <VideoList
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos={this.state.videos}/>
       </div>
-    );
+      )
   }
 }
 
---
+++ src/Components/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Youtube = function(){
+    return (
+        <div className="youtube">
+        <h1>MOVIETUBE</h1>
+
+        </div>
+        )
+}
+export default Youtube ;
